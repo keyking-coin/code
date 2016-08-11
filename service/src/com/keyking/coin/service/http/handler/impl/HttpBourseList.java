626765cc94ee7a72ca5f44878451d3b3c42f4500
tanyong <tan.y@joymeng.com>
2016-08-11 22:44:06 +0800

    更新

--- service/src/com/keyking/coin/service/http/handler/impl/HttpBourseList.java
+++ service/src/com/keyking/coin/service/http/handler/impl/HttpBourseList.java
@@ -1,6 +1,9 @@
 package com.keyking.coin.service.http.handler.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.keyking.coin.service.domain.bourse.BourseInfo;
 import com.keyking.coin.service.http.handler.HttpHandler;
@@ -9,14 +12,25 @@ import com.keyking.coin.service.http.response.HttpResponseMessage;
 import com.keyking.coin.util.JsonUtil;
 
 public class HttpBourseList extends HttpHandler {
+	//http://139.196.30.53:32104/HttpDealDel?type=1
 	@Override
 	public void handle(HttpRequestMessage request, HttpResponseMessage response) {
 		response.setContentType("text/plain");
 		response.setResponseCode(HttpResponseMessage.HTTP_STATUS_SUCCESS);
 		String str = request.getParameter("type");//1只在文交所导航,2文交所导航加下拉列表,3文交所导航加下拉列表加热门文交所
 		byte type = Byte.parseByte(str);
-		List<BourseInfo> infos = DB.getBourseDao().load(type);
-		String result = infos == null ? "[]" : JsonUtil.ObjectToJsonString(infos);
-		message(request,response,result);
+		List<BourseInfo> infos = null;
+		if (type == 1){
+			infos = DB.getBourseDao().load();
+		}else{
+			infos = DB.getBourseDao().load(type);
+		}
+		if (infos == null){
+			infos = new ArrayList<BourseInfo>();
+		}
+		Map<String,Object> data = new HashMap<String,Object>();
+		data.put("list",infos);
+		String result = JsonUtil.ObjectToJsonString(data);
+		data(request,response,result);
 	}
 }
