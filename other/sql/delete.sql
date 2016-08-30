c5f4ec81c30ce24f4bd9aff2f8050a8e543f69eb
tanyong <tan.y@joymeng.com>
2016-08-30 10:08:22 +0800

    3

---
+++ other/sql/delete.sql
@@ -0,0 +1,17 @@
+delete from city;
+delete from cityMove;
+delete from mapBarracks;
+delete from mapFortress;
+delete from mapObj;
+delete from npc_city;
+delete from resources;
+delete from role;
+delete from roleExpedite;
+delete from roleGarrison;
+delete from unionBody;
+delete from unionBuild;
+delete from unionMember;
+delete from shopLimit;
+delete from statisticData;
+delete from worldStaticData;
+delete from actvt;
