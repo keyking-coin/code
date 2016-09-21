c99fadae62e263e2d2692fe40051ef16bf2317d9
tanyong <tan.y@joymeng.com>
2016-09-21 09:10:34 +0800

    加入线程池

--- service/src/com/keyking/coin/service/Service.java
+++ service/src/com/keyking/coin/service/Service.java
@@ -3,10 +3,16 @@ package com.keyking.coin.service;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.executor.ExecutorFilter;
 import org.apache.mina.transport.socket.SocketAcceptor;
 import org.apache.mina.transport.socket.SocketSessionConfig;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
@@ -37,6 +43,7 @@ public class Service implements Instances{
 	public static String VERSION = "1.0.1";
 	
 	public static String APK_URL;
+
 	
 	public static void main(String[] args) {
         try {
@@ -56,6 +63,7 @@ public class Service implements Instances{
             config.setSoLinger(0); //这个是根本解决问题的设置 
             DefaultIoFilterChainBuilder chain = acceptor.getFilterChain();
             chain.addLast("codec",new ProtocolCodecFilter(new MessageCodecFactory()));
+            chain.addLast("threadPool",new ExecutorFilter(configureExecutorService()));
             acceptor.setHandler(NET);
             InetAddress address = InetAddress.getByName(URL);
 			acceptor.bind(new InetSocketAddress(address,PORT));
@@ -85,6 +93,28 @@ public class Service implements Instances{
 			VERSION   = XmlUtils.getAttribute(service,"version");
 			APK_URL   = XmlUtils.getAttribute(service,"url");
 		}
+	}
+	
+	public static ExecutorService configureExecutorService(){
+		String str1 = System.getProperty("core.thread.num");
+		if (str1 == null){
+			return Executors.newCachedThreadPool();
+		}
+		int i = Integer.parseInt(str1);
+		int j = i;
+		String str2 = System.getProperty("max.thread.num");
+		if (str2 != null){
+			j = Integer.parseInt(str2);
+		}
+		if (j <= i){
+			return Executors.newFixedThreadPool(i);
+		}
+		String str3 = System.getProperty("task.queue.size");
+		if (str3 == null){
+			return new ThreadPoolExecutor(i, j, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(100));
+		}
+		int k = Integer.parseInt(str3);
+		return new ThreadPoolExecutor(i, j, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(k));
 	}
 }
  
